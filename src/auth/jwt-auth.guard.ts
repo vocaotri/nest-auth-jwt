314468afcc2c6f73f76beda14109e0502418de5c
Tri Vo <v_tri@gonosen.asia>
2022-07-28 16:32:54 +0700

    update jwt auth guard

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -13,6 +13,8 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     handleRequest(err, user, info) {
         if(this.role == 'user' && user.is_admin)
             throw new ForbiddenException()
+        if(this.role == 'admin' && !user.is_admin)
+            throw new ForbiddenException()
         
         // You can throw an exception based on either "info" or "err" arguments
         if (err || !user) {
