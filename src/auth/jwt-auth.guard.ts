2d0b197b7dcddbdbac187e58b5077b8b52ba6f8c
Tri Vo <v_tri@gonosen.asia>
2022-07-28 16:27:01 +0700

    update jwt auth guard

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,14 +1,19 @@
-import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException, ForbiddenException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+    constructor(private role:string = 'user'){
+        super();
+    }
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        // return false;
         return super.canActivate(context);
     }
     handleRequest(err, user, info) {
+        if(this.role == 'user' && user.is_admin)
+            throw new ForbiddenException()
+        
         // You can throw an exception based on either "info" or "err" arguments
         if (err || !user) {
             throw err || new UnauthorizedException();
